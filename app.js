62ffc5920f5ddd8706b3709cfc1fa9f9991e1c1b
Anıl Kılıç <anlklc@gmail.com>
2018-03-09 23:50:06 +0300

    closes #7; User route

--- app.js
+++ app.js
@@ -49,22 +49,23 @@ app.get('/moderator/:moderatorName', (req, res) => {
     console.log(reviews);
 });
 
+app.get('/user/:username', (req, res) => {
+    var contributions = [];
+    console.log(req.params.username);
+    utopian.getPosts({
+        section: 'author',
+        category: 'graphics',
+        author: req.params.username,
+        limit: 10
+    }).then((posts) => {
+        for(i = 0; i < posts.results.length; i++) {
+            contributions.push(posts.results[i])
+        }
+        res.render('home', {
+            title: 'User',
+            posts: contributions
+        });
+    });
 
-// app.get('/moderator/:moderatorName', (req, res) => {
-//     var reviews = [];
-
-//     utopian.getPosts({
-//         moderator: moderatorName,
-//         limit: 10
-//     }).then((posts) => {
-//         for(i = 0; i < posts.results.length; i++) {
-//             reviews.push(posts.results[i])
-//         }
-//         res.render('home', {
-//             title: 'Latest Graphics Contributions',
-//             posts: reviews
-//         });
-//     });
-
-//     console.log(reviews);
-// });
+    console.log(contributions);
+});
